5912a599bccb0cab543451e6eb5c625623d769fa
Daniel <danieljunior@id.uff.br>
2013-08-25 15:48:21 -0300

    Configurado para ligar e desligar o gps quando for preciso

--- src/com/djia/infoandroid/MainActivity.java
+++ src/com/djia/infoandroid/MainActivity.java
@@ -3,8 +3,10 @@ package com.djia.infoandroid;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -18,6 +20,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements LocationListener {
 	private LocationManager lm;
 	private double longitude, latitude;
+	private String provider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +41,11 @@ public class MainActivity extends Activity implements LocationListener {
 		TextView disp = (TextView) findViewById(R.id.dispositivo);
 		disp.setText("ID: " + id);
 		TextView bat = (TextView) findViewById(R.id.bateria);
-		bat.setText("Nível de bateria: " + level+"%");
-		TextView lat_long = (TextView) findViewById(R.id.lat_long);
-		lat_long.setText("Latitude e Longitude: "+latitude+" / "+longitude);
+		bat.setText("Nível de bateria: " + level + "%");
+		TextView lat = (TextView) findViewById(R.id.lat);
+		lat.setText("Latitude : " + latitude);
+		TextView longi = (TextView) findViewById(R.id.longi);
+		longi.setText("Longitude: " + longitude);
 		TextView op = (TextView) findViewById(R.id.operadora);
 		op.setText("Operadora: " + operadora);
 		TextView sinal = (TextView) findViewById(R.id.sinal);
@@ -54,12 +59,51 @@ public class MainActivity extends Activity implements LocationListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-@Override
-protected void onResume() {
-	// TODO Auto-generated method stub
-	lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this);
-	super.onResume();
-}
+
+	private void turnGPSOn() {
+		// se o gps está desligado, então ligo
+		provider = Settings.Secure.getString(getContentResolver(),
+				Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+		if (!provider.contains("gps")) {
+			final Intent poke = new Intent();
+			poke.setClassName("com.android.settings",
+					"com.android.settings.widget.SettingsAppWidgetProvider");
+			poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+			poke.setData(Uri.parse("3"));
+			sendBroadcast(poke);
+		}
+	}
+
+	private void turnGPSOff() {
+		provider = Settings.Secure.getString(getContentResolver(),
+				Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+		// se o gps está ligado , então o desligo
+		if (provider.contains("gps")) {
+			final Intent poke = new Intent();
+			poke.setClassName("com.android.settings",
+					"com.android.settings.widget.SettingsAppWidgetProvider");
+			poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+			poke.setData(Uri.parse("3"));
+			sendBroadcast(poke);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		//Estou ligando o  gps forçadamente sem o consentimento do utilizador;
+		turnGPSOn();
+		//caso queira que ele msm faça esta ação devo utilizar:
+				/*
+				Intent in = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);   
+				startActivity(in);
+				*/
+		// Aqui você define quando quer que as informações sejam atualizadas, no
+		// meu caso eu coloquei a cada 0 segundos ou 0 metros
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this);
+		super.onResume();
+	}
+
 	@Override
 	public void onLocationChanged(Location location) {
 		longitude = location.getLongitude();
@@ -70,13 +114,12 @@ protected void onResume() {
 	@Override
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
-		Toast.makeText(this, "Desligado", Toast.LENGTH_SHORT).show();
+
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
 		// TODO Auto-generated method stub
-		Toast.makeText(this, "Ligado", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
@@ -84,9 +127,11 @@ protected void onResume() {
 		// TODO Auto-generated method stub
 
 	}
-@Override
-protected void onPause() {
-	// TODO Auto-generated method stub
-	super.onPause();
-}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		turnGPSOff();
+		super.onStop();
+	}
 }
